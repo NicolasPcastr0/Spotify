3c121f12621796fee76515d696e09df47aff5dac
NicolasPcastr0 <npcastro08@gmail.com>
2025-06-06 19:22:59 -0300

    Criação de sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { assets } from "../assets/assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex ">
+        <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+            <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <img className="w-6" src={assets.home_icon} alt="" />
+                <p className="font-bold">Home</p>
+            </div>
+            <div className="flex items-center gap-3 pl-8 cursor-pointer">
+                <img className="w-6" src={assets.search_icon} alt="" />
+                <p className="font-bold">Pesquisa</p>
+            </div>
+        </div>
+        <div className="bg-[#121212] h-[85%] rounded">
+          <div className="p-4 flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <img className="w-8" src={assets.stack_icon} alt="" />
+              <p className="font-semibold">Biblioteca</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <img className="w-6" src={assets.arrow_icon} alt="" />
+              <img className="w-6" src={assets.plus_icon} alt="" />
+            </div>
+          </div>
+          <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-3 pl-4">
+            <h1>Crie sua Playlist</h1>
+            <button className="bg-[#1DB954] text-white py-1.5 px-4 rounded-full cursor-pointer">Clique aqui</button>
+          </div>
+          <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-3 pl-4 mt-4">
+            <h1>Ache podcasts do seu interesse</h1>
+            <button className="bg-[#1DB954] text-white py-1.5 px-4 rounded-full cursor-pointer">Pesquisar podcast</button>
+          </div>
+        </div>
+    </div>
+  );
+};
+ 
+export default Sidebar;
