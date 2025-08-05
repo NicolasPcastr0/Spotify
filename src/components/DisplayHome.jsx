3102b0f8e28d3d7887edd929375a6c966485fd34
NicolasPcastr0 <npcastro08@gmail.com>
2025-08-05 12:11:21 -0300

    Criação álbuns

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -3,6 +3,8 @@ import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets/assets"; 
 import AlbumItem from "./AlbumItem"; 
 import CustomScrollContainer from "./CustomScrollContainer"; 
+import { songsData } from "../assets/assets/assets";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
     return (
@@ -15,8 +17,26 @@ const DisplayHome = () => {
                         {albumsData.map((item,index) => (
                             <AlbumItem key={index}
                             name={item.name}
+                            desc={item.desc}
+                            id={item.id}
                             image={item.image}
-                            desc={item.desc} />
+                             />
+                        ))}
+                    </div>
+                </CustomScrollContainer>
+            </div>
+
+            <div className="mb-4">
+                <h1 className="font-bold my-5 text-2xl">Tocados recentemente</h1>
+                <CustomScrollContainer>
+                    <div className="flex">
+                        {songsData.map((item,index) => (
+                            <SongItem key={index}
+                            name={item.name}
+                            desc={item.desc}
+                            id={item.id}
+                            image={item.image}
+                             />
                         ))}
                     </div>
                 </CustomScrollContainer>
